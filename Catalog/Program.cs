f9d6659df0b7985dead6276151d59aa519e88bc3
kristabelk <kristabelkhoo@gmail.com>
2023-06-14 14:22:53 +0800

    kristabel

--- Catalog/Program.cs
+++ Catalog/Program.cs
@@ -2,10 +2,12 @@
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddTransient<ICategoryRepository, CatetgoryInMemoryRepo>();
+
+builder.Services.AddControllers();
+
 var app = builder.Build();
     
 app.MapGet("/", () => "Catalog Module");
-
+app.MapControllers();
 app.Run();
 
-//test
